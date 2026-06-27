a01c2401d9de7d2bf12f3caf0432e698f8ec41b4
cameronlewisscott136-dev <cameronlewisscott136@gmail.com>
2026-06-27 22:32:00 +0300

    Remove getPaymentByOrderId route from payment.js
    Removed the 'getPaymentByOrderId' route from payment.js.

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -1,23 +1,14 @@
-// routes/payment.js
+// backend/src/routes/payment.js
 const express = require('express');
 const router = express.Router();
-const {
-    initiatePayment,
-    paymentCallback,
-    checkPaymentStatus,
-    getPaymentByOrderId
+const { 
+  initiatePayment, 
+  paymentCallback, 
+  checkPaymentStatus 
 } = require('../controllers/paymentController');
 
-// Initiate payment
 router.post('/initiate', initiatePayment);
-
-// Payment callback (M-Pesa)
 router.post('/callback', paymentCallback);
-
-// Check payment status
 router.get('/status/:checkoutRequestID', checkPaymentStatus);
-
-// Get payment by order ID
-router.get('/order/:orderId', getPaymentByOrderId);
 
 module.exports = router;
