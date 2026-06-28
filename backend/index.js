67c3bc35414f58b459c6ad0e4ffd2bc42a8c4139
cameronlewisscott136-dev <cameronlewisscott136@gmail.com>
2026-06-28 07:43:42 -0500

    Update CORS allowed origins
    Added a new allowed origin to the CORS configuration.

--- backend/index.js
+++ backend/index.js
@@ -41,7 +41,8 @@ app.use(cors({
         'https://graceful-hotteok-15efe8.netlify.app',
         'https://graceful-hotteok-15efe8.netlify.app/api',
         'https://summertides-eight.vercel.app',
-        'https://summertides-8xx07vmzk-cameronlewisscott136-devs-projects.vercel.app'
+        'https://summertides-8xx07vmzk-cameronlewisscott136-devs-projects.vercel.app',
+        'https://summertides-nbcy0qhhf-cameronlewisscott136-devs-projects.vercel.app'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
